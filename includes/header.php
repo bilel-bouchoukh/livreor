3619428a12a27692b8c19d45e70f4995faf44856
bilel-bouchoukh <bilel.bouchoukh@laplateforme.io>
2025-02-17 14:57:12 +0100

    feat

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <a href="#">Page D'acceuil</a>
+            <a href="#">Connexion</a>
+            <a href="#">Inscription</a>
+            <a href="#">Livre d'Or</a>
+        </div>
+    </header>
+    
+</body>
+</html>
